d827c886b8ed69c046a08de450b5975b60884eaa
Alexander Hoinville <ahoin001@gmail.com>
2022-03-26 23:09:20 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+
+# Importing all the necessary modules for Python YouTube Video Downloader project
+from tkinter import *
+from tkinter import messagebox as mb
+from pytube import YouTube
+
+# Defining the downloader function for YouTube Video Downloader project using Python
+def downloader(link, directory, filename):
+    yt_link = link.get()
+    save_path = directory.get()
+    aftersave_filename = filename.get()
+    try:
+        yt = YouTube(yt_link)
+        video = yt.streams.first()
+        video.download(save_path, aftersave_filename)
+    except:
+        mb.showerror('Error', 'Connection Error! You are offline!')
+def reset(l_strvar, d_strvar, fn_strvar):
+    l_strvar.set('')
+    d_strvar.set('')
+    fn_strvar.set('')
